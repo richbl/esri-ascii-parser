d1bdc05d5b3093ae5634e17dc37e12ff2502cdfc
Rich <richbl@users.noreply.github.com>
2014-12-16 19:44:27 -0800

    Added year to copyright details

--- esri-ascii-parser.go
+++ esri-ascii-parser.go
@@ -1,5 +1,5 @@
 //
-// Copyright (C) Business Learning Incorporated (www.businesslearninginc.com)
+// Copyright (C) 2014 Business Learning Incorporated (www.businesslearninginc.com)
 //
 // Use of this source code is governed by an MIT-style license
 // that can be found in the LICENSE file
